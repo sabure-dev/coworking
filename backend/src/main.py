7286d093e974a8bf17c25e222fc4971a9469c7cb
sabure <dev.sabure@gmail.com>
2024-07-20 16:32:41 +0300

    mounting static media

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,7 +9,7 @@ from project import project
 from fastapi.middleware.cors import CORSMiddleware
 import redis
 
-rd = redis.Redis(host='red-cqb6o92j1k6c73amhc10', port=6379, db=0)
+rd = redis.Redis(host='redis', port=6379, db=0)
 
 app = FastAPI(title="Coworking")
 
