bbb69a51c66c2e1f1a163244974e500b14ffeeab
sabure <dev.sabure@gmail.com>
2024-07-23 23:20:36 +0300

    upload project files

--- frontend/coworking/src/components/Project/My/myProject.jsx
+++ frontend/coworking/src/components/Project/My/myProject.jsx
@@ -75,7 +75,7 @@ function MyProjectPage() {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', filename);
+            link.setAttribute('download', 'qwe.zip');
             document.body.appendChild(link);
             link.click();
 
