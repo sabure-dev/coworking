f8d937163e37bdbe10b0e844435487571f908798
sabure <dev.sabure@gmail.com>
2024-07-23 22:31:15 +0300

    image upload news

--- frontend/coworking/src/components/Project/My/myProject.jsx
+++ frontend/coworking/src/components/Project/My/myProject.jsx
@@ -69,7 +69,7 @@ function MyProjectPage() {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', 'file.pdf');
+            link.setAttribute('download', response.headers.get('Content-Disposition'));
             document.body.appendChild(link);
             link.click();
 
