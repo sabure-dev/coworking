31cd30b34890fc4a2139f159f98b5f8dc3bc27d6
sabure-dev <dev.sabure@gmail.com>
2025-02-17 20:16:45 +0300

    fix cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -24,8 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router, prefix='/api')
